4df3f8f8ba088cbea920fd75b6fd0112203441d3
liyongjun <liyongjun@crf-tech.com>
2022-05-24 15:16:14 +0800

    study

--- 列表/列表map遍历.py
+++ 列表/列表map遍历.py
@@ -15,3 +15,14 @@ for i in array:
         print(True)
     else:
         print(False)
+
+# python 2  返回列表
+print(map(lambda x: x ** 2, [1, 2, 3, 4, 5])) # [1, 4, 9, 16, 25]
+print(map(lambda x, y: x + y, [1, 3, 5, 7, 9], [2, 4, 6, 8, 10])) # 列表推导式
+# python 3 返回迭代器
+print(list(map(lambda x: x ** 2, [1, 2, 3, 4, 5]))) # [1, 4, 9, 16, 25]
+print(list(map(lambda x, y: x + y, [1, 3, 5, 7, 9], [2, 4, 6, 8, 10]))) # 可以接受多个参数
+
+print(list(map(lambda x: x * x / 100, [1, 2, 3, 4, 5])))
+total_used = sum(map(lambda x: x * x / 100, [1, 2, 3, 4, 5]))
+print(total_used)
