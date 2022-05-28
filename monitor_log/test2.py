6730fbf911eadb06580e223a99e6282e0c90436e
liyongjun <1477676296@qq.com>
2022-05-28 17:18:14 +0800

    monitor

---
+++ monitor_log/test2.py
@@ -0,0 +1,11 @@
+# 测试代码
+import time
+
+log_path = "test/2.txt"
+
+with open(log_path, mode='a', encoding='utf8') as f1:
+    while 1:
+        f1.write("jjj\n")
+        print("jjj")
+        time.sleep(1)
+        f1.flush()
