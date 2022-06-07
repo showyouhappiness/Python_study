6240cf31b0aca35d152f2aa5264145b15153ebf4
1477676296@qq.com <1477676296@qq.com>
2022-06-08 00:42:46 +0800

    list 插入 最后一个 双层for循环

---
+++ 列表/test.py
@@ -0,0 +1,29 @@
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles)
+# print(bicycles[0].title())
+# print(bicycles[-1])
+#
+# bicycles[0] = "自行车第一个元素";
+# print(bicycles)
+# bicycles[-1] = "自行车最后一个元素"
+# print(bicycles)
+# bicycles.append("飞机")
+# print(bicycles)
+# bicycles.insert(0, "替换第一个元素")
+# print(bicycles)
+# bicycles.insert(-1, "替换最后一个元素")
+# print(bicycles)
+#
+# newBicycles = bicycles;
+# print(newBicycles)
+# newBicycles.insert(0, "我是0")
+# newBicycles.insert(-1, "我是-1")
+#
+# print(newBicycles)
+
+a = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+b = [j for i in a for j in i]
+print(b)
+import pyecharts
+
+
