8003aa226b3227d6892e783c2870531363549ecb
liyongjun <liyongjun@crf-tech.com>
2023-06-26 15:28:03 +0800

    删除无用代码

--- 并发/asyncio和aiohttp/server.py
+++ 并发/asyncio和aiohttp/server.py
@@ -2,13 +2,14 @@ from aiohttp import web
 
 
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
+    name = request.match_info.get('name', "Anonymous")  # 从请求中获取参数
     text = "Hello, " + name
     return web.Response(text=text)
 
-app = web.Application()
+
+app = web.Application()  # 创建一个应用
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/{name}', handle)])  # 添加路由
 
 if __name__ == '__main__':
-    web.run_app(app)
+    web.run_app(app)  # 运行应用
